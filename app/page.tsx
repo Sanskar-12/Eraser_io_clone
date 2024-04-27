a7c4d1e293221b0fd7b1a7935a055709f7c9c55b
Sanskar-12 <sanskarv2004@gmail.com>
2024-04-27 19:33:06 +0530

    authentication completed

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,22 @@
-import { Button } from "@/components/ui/button";
+"use client";
+
+import { useKindeBrowserClient } from "@kinde-oss/kinde-auth-nextjs";
+import Header from "./_components/Header";
+import Hero from "./_components/Hero";
+import { useEffect } from "react";
 
 const page = () => {
+  const { user } = useKindeBrowserClient();
+
+  useEffect(() => {
+    console.log(user);
+  }, [user]);
+
   return (
-    <div>
-      <h1>Hello World</h1>
-      <Button>Hello</Button>
-    </div>
+    <>
+      <Header />
+      <Hero />
+    </>
   );
 };
 
