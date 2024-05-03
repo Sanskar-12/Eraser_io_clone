aa9f7cb0193ae71a53107e3783e6526bff224f73
Sanskar-12 <sanskarv2004@gmail.com>
2024-05-03 07:50:20 +0530

    Document part completed

--- app/(routes)/workspace/_components/Editor.tsx
+++ app/(routes)/workspace/_components/Editor.tsx
@@ -1,9 +1,40 @@
+// @ts-nocheck
 "use client";
 
-import React, { useEffect } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import EditorJS from "@editorjs/editorjs";
+import Header from "@editorjs/header";
+import List from "@editorjs/list";
+import Checklist from "@editorjs/checklist";
+import Paragraph from "@editorjs/paragraph";
+
+const rawDocument = {
+  time: 1550476186479,
+  blocks: [
+    {
+      data: {
+        text: "Document Name",
+        level: 2,
+      },
+      id: "123",
+      type: "header",
+    },
+    {
+      data: {
+        level: 4,
+      },
+      id: "1234",
+      type: "header",
+    },
+  ],
+  version: "2.8.1",
+};
 
 const Editor = () => {
+  const ref = useRef<EditorJS>();
+
+  const [document, setDocument] = useState(rawDocument);
+
   useEffect(() => {
     initEditor();
   }, []);
@@ -13,8 +44,34 @@ const Editor = () => {
       /**
        * Id of Element that should contain Editor instance
        */
+      tools: {
+        header: {
+          class: Header,
+          config: {
+            placeholder: "Enter a Header",
+          },
+        },
+        list: {
+          class: List,
+          inlineToolbar: true,
+          config: {
+            defaultStyle: "unordered",
+          },
+        },
+        checklist: {
+          class: Checklist,
+          inlineToolbar: true,
+        },
+        paragraph: {
+          class: Paragraph,
+          inlineToolbar: true,
+        },
+      },
       holder: "editorjs",
+      data: document,
     });
+
+    ref.current = editor;
   };
 
   return <div id="editorjs"></div>;
