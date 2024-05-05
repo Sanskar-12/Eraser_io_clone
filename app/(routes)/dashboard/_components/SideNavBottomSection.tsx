10ba055bfba3f688ecaeca4ff88405d0a1fb0d63
Sanskar-12 <sanskarv2004@gmail.com>
2024-05-05 09:45:57 +0530

    Project Completed

--- app/(routes)/dashboard/_components/SideNavBottomSection.tsx
+++ app/(routes)/dashboard/_components/SideNavBottomSection.tsx
@@ -14,6 +14,8 @@ import {
   DialogTrigger,
 } from "@/components/ui/dialog";
 import { Input } from "@/components/ui/input";
+import constants from "@/app/_constants/constant";
+import PricingDialog from "./PricingDialog";
 
 const SideNavBottomSection = ({ onFileCreate, totalFiles }: any) => {
   const menuList = [
@@ -59,30 +61,34 @@ const SideNavBottomSection = ({ onFileCreate, totalFiles }: any) => {
             New File
           </Button>
         </DialogTrigger>
-        <DialogContent>
-          <DialogHeader>
-            <DialogTitle>Create New File</DialogTitle>
-            <DialogDescription>
-              <Input
-                placeholder="Enter File Name"
-                className="mt-3"
-                onChange={(e) => setFileInput(e.target.value)}
-              />
-            </DialogDescription>
-          </DialogHeader>
-          <DialogFooter>
-            <DialogClose asChild>
-              <Button
-                type="button"
-                className="bg-blue-600 hover:bg-blue-700"
-                disabled={!(fileInput && fileInput.length > 0)}
-                onClick={() => onFileCreate(fileInput)}
-              >
-                Create
-              </Button>
-            </DialogClose>
-          </DialogFooter>
-        </DialogContent>
+        {totalFiles < constants.MAX_FREE_FILE ? (
+          <DialogContent>
+            <DialogHeader>
+              <DialogTitle>Create New File</DialogTitle>
+              <DialogDescription>
+                <Input
+                  placeholder="Enter File Name"
+                  className="mt-3"
+                  onChange={(e) => setFileInput(e.target.value)}
+                />
+              </DialogDescription>
+            </DialogHeader>
+            <DialogFooter>
+              <DialogClose asChild>
+                <Button
+                  type="button"
+                  className="bg-blue-600 hover:bg-blue-700"
+                  disabled={!(fileInput && fileInput.length > 0)}
+                  onClick={() => onFileCreate(fileInput)}
+                >
+                  Create
+                </Button>
+              </DialogClose>
+            </DialogFooter>
+          </DialogContent>
+        ) : (
+          <PricingDialog />
+        )}
       </Dialog>
 
       {/* Progress Bar */}
@@ -93,7 +99,8 @@ const SideNavBottomSection = ({ onFileCreate, totalFiles }: any) => {
         ></div>
       </div>
       <h2 className="text-[12px] mt-3">
-        <strong>{totalFiles}</strong> out of <strong>5</strong> files used
+        <strong>{totalFiles}</strong> out of{" "}
+        <strong>{constants.MAX_FREE_FILE}</strong> files used
       </h2>
       <h2 className="text-[12px] mt-1">
         Upgrade your plan for unlimited access.
---
+++ app/_constants/constant.ts
@@ -0,0 +1,5 @@
+const constants = {
+  MAX_FREE_FILE: 5,
+};
+
+export default constants;
